e7747a40e951109cc97f6d738d51494018d60896
Shlok Stampwala <shlokstampwala@gmail.com>
2025-09-12 22:34:03 +0530

    Add files via upload

---
+++ Daily Challenge/3227_Vowels_game_in_a_String.java
@@ -0,0 +1,16 @@
+class Solution {
+    //@Shlok Stampwala
+    public boolean doesAliceWin(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (isVowel(c)) {
+                return true; //if vowel
+            }
+        }
+        return false;
+    }
+//isvowel for checking 
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+}
